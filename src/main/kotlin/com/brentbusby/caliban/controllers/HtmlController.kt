ad11145539f0816e98fdbcd7bac613960854534b
Brent Busby <brent.busby@yahoo.com>
2020-11-22 13:46:43 -0600

    add homepage template, controller

---
+++ src/main/kotlin/com/brentbusby/caliban/controllers/HtmlController.kt
@@ -0,0 +1,17 @@
+package com.brentbusby.caliban.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun home(model: Model): String {
+        model["title"] = "Home"
+        return "home"
+    }
+
+}
